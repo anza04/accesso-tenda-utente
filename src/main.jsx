f11f47d3183e25bf10ef45732f348d2976b8519b
anza04 <64538739+anza04@users.noreply.github.com>
2023-05-27 10:37:47 +0200

    coming soon mode re-activated

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Homepage />,
+    element: <ComingSoon />,
   },
   {
     path: '/signup',
