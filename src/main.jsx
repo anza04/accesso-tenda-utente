8ba2e7ae8037eecdc4415c0cdaa6ff611a224317
Federico Anzani <federico.anzani04@gmail.com>
2023-05-30 08:32:45 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <ComingSoon />,
+    element: <Homepage />,
   },
   {
     path: '/signup',
